de368ea5fb5787d01e3ae6a17980a21220b3802f
GusBedasi <ggustavobedasi@gmail.com>
2021-10-31 00:36:53 -0300

    first commit for func using with style :glasses

---
+++ FuncExamplePoc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FuncExamplePoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindOne(a => a.Age == 18).Name);
+        }
+
+        static Student FindOne(Func<Student, bool> a)
+        {
+            var students = new List<Student>()
+            {
+                new Student("Nicolle",13),
+                new Student("Gustavo",14),
+                new Student("Silene",15),
+                new Student("Melissa",16),
+                new Student("Chancha",17),
+                new Student("Fabão",18)
+            };
+
+            return students.FirstOrDefault(a);
+        }
+    }
+
+    class Student
+    {
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
